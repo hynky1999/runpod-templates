e79b2b60f09519e4e41529e6175aa29ef2e02181
EC2 Default User <ec2-user@ip-172-31-16-254.eu-central-1.compute.internal>
2023-12-26 17:32:39 +0000

    hh

--- pytorch_with_s3_backup/scripts/entrypoint.sh
+++ pytorch_with_s3_backup/scripts/entrypoint.sh
@@ -4,5 +4,12 @@
 echo "Downloading folder from S3..."
 aws s3 sync s3://$S3_BUCKET/$S3_FOLDER $S3_FOLDER
 
+# Export the env variables to .bashrc
+printenv | grep -E '^AWS_|^S3_' | awk -F = '{ print "export " $1 "=\"" $2 "\"" }' >> /etc/aws_environment
+echo 'source /etc/aws_environment' >> ~/.bashrc
+
+
+
+
 # Run the main process
 exec "$@"
